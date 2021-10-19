6c71272d6205cfead7dccb23a394107c7c79e249
Paul Nilsson <paul.nilsson@cern.ch>
2021-10-19 17:10:08 +0200

    Added debug info

--- utilities.py
+++ utilities.py
@@ -262,7 +262,9 @@ def wait_until_deployment(name=None, state=None, timeout=120, namespace=None, de
     port_pattern = r'([0-9]+)\:.'  # e.g. 80:30525/TCP
     while processing and (now - starttime < timeout):
 
-        exitcode, stdout, stderr = execute("kubectl get %s %s --namespace=%s" % (podtype, name, namespace))
+        cmd = "kubectl get %s %s --namespace=%s" % (podtype, name, namespace)
+        logger.debug('executing cmd=\'%s\'', cmd)
+        exitcode, stdout, stderr = execute(cmd)
         logger.debug(exitcode)
         logger.debug(stdout)
         logger.debug(stderr)
